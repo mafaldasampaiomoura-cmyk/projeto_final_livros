e47db5f29bc8b07c27771d0ea26b26e42ae0527e
mafaldasampaiomoura-cmyk <mafaldasampaiomoura@gmail.com>
2026-03-18 09:41:55 +0000

    Test CI failure

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,7 +1,7 @@
-import { Component, inject, ChangeDetectorRef } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+// import { Component, inject, ChangeDetectorRef } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
